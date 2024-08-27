b149178cf393d8037a56a2865f7439195231c944
gjwldddd <heoj18908@gmail.com>
2024-08-27 23:41:34 +0900

    [Feat] app.js FileUploadAft.jsx 연결

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { UserPage } from './pages/customer/mypage/UserPage';
 import { UserPageAft } from './pages/customer/mypage/UserPageAft';
 import { KitchenProfile } from './pages/customer/mypage/KitchenProfile';
 import { FileUpload } from './pages/customer/mypage/FileUpload';
+import { FileUploadAft } from './pages/customer/mypage/FileUploadAft';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme/theme';
 import Reserve from './pages/chef/Reserve';
@@ -31,6 +32,7 @@ function App() {
             <Route path="/userpageaft" element={<UserPageAft />} theme={theme}/>
             <Route path="/kitchenprofile" element={<KitchenProfile />} theme={theme}/>
             <Route path="/fileupload" element={<FileUpload />} theme={theme}/>
+            <Route path="/fileuploadaft" element={<FileUploadAft />} theme={theme}/>
         </Route>
       </Routes>
     </ThemeProvider>
