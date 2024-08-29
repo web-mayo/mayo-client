71fd3e97e17168092da19291d7d04982135f50d3
gjwldddd <heoj18908@gmail.com>
2024-08-29 20:10:16 +0900

    [Feat] 요리사 리스트 하단 바

--- src/pages/customer/customerboard/Board.jsx
+++ src/pages/customer/customerboard/Board.jsx
@@ -120,7 +120,18 @@ export const Board = () => {
               </ChefBox>
             </ChefMiddle>
             <ChefBottom>
-
+              <Box>
+                <NumNow>1</NumNow>
+                <Num>2</Num>
+                <Num>3</Num>
+                <Num>...</Num>
+                <Num>67</Num>
+                <Num>68</Num>
+                <NextSign></NextSign>
+                <NextSign2>
+                  <NextSign></NextSign><NextSign></NextSign>
+                </NextSign2>
+              </Box>
             </ChefBottom>
           </Container5>
         </Container3>
@@ -407,4 +418,41 @@ const ExtraBox = styled.div`
 const ChefBottom = styled.div`
   width: 864px;
   height: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Box = styled.div`
+  width: 350px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const NumNow = styled.div`
+  cont-size: 17px;
+  border-radius: 10px;
+  background-color: #FA7C15;
+  padding: 8px 15px;
+  color: white;
+`
+
+const Num = styled.div`
+  font-size: 17px;
+  padding: 8px 15px;
+`
+
+const NextSign = styled.div`
+  content: '';
+  width: 5.5px;
+  height: 5.5px;
+  border-top: 1px solid;
+  border-right: 1px solid;
+  transform: rotate(45deg);
+`
+
+const NextSign2 = styled.div`
+  display: flex;
+  padding-left: 20px;
 `
