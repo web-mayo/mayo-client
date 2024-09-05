3f9dc6d09752d3b41cd10f45dbf17f3e7b9c6e16
hwiy01 <vdocom02@naver.com>
2024-09-05 22:09:41 +0900

    [Feat] 요리사 게시판 캘린더 부분 구현

--- src/pages/chef/ChefBoard.jsx
+++ src/pages/chef/ChefBoard.jsx
@@ -1,75 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Title } from '../../components/Title'
 import styled from 'styled-components'
-import { CalenderPart } from '../../components/CalenderPart'
+import { CalendarPart } from '../../components/CalendarPart'
 
 
 export const ChefBoard = () => {
+  const [showSearchSec, setShowSearchSec] = useState(false);
+  const [searchDate, setSearchDate] = useState();
+
+  const handleShowSearch = () => {
+    if(searchDate){
+      setShowSearchSec(true);
+    }
+  }
+
   return (
     <>
     <ChefBoardContainer>
       <Title title={'게시판'} backgroundColor={'white'}></Title>
-      <Title title={'마이요리사 찾기'} subTitle={'마이요리사님 일정에 맞는 홈파티를 찾아보세요!'}></Title>
-        <CalenderContainer>
-          <CalenderPart />
-          <CalenderSearchBtn>검색</CalenderSearchBtn>
-        </CalenderContainer>
-     <SearchSection>
-      <Title title={'홈파티찾기'} subTitle={'요리사 모집 중인 홈파티에 신청해보세요!'} backgroundColor={'white'}></Title>
-      <SearchSectionContainer>
-        <SearchCard>
-          <SearchCardTitle>
-            <SearchCardPlace>용산구</SearchCardPlace>
-            <SearchCardDate>2024/08/31</SearchCardDate>
-          </SearchCardTitle>
-          <SearchCardIntro>한줄소개</SearchCardIntro>
-          <SearchCardPeople>0명</SearchCardPeople>
-        </SearchCard>
-        <SearchCard>
-          <SearchCardTitle>
-            <SearchCardPlace>용산구</SearchCardPlace>
-            <SearchCardDate>2024/08/31</SearchCardDate>
-          </SearchCardTitle>
-          <SearchCardIntro>한줄소개</SearchCardIntro>
-          <SearchCardPeople>0명</SearchCardPeople>
-        </SearchCard>
-        <SearchCard>
-          <SearchCardTitle>
-            <SearchCardPlace>용산구</SearchCardPlace>
-            <SearchCardDate>2024/08/31</SearchCardDate>
-          </SearchCardTitle>
-          <SearchCardIntro>한줄소개</SearchCardIntro>
-          <SearchCardPeople>0명</SearchCardPeople>
-        </SearchCard>
-        <SearchCard>
-          <SearchCardTitle>
-            <SearchCardPlace>용산구</SearchCardPlace>
-            <SearchCardDate>2024/08/31</SearchCardDate>
-          </SearchCardTitle>
-          <SearchCardIntro>한줄소개</SearchCardIntro>
-          <SearchCardPeople>0명</SearchCardPeople>
-        </SearchCard>
-        <SearchCard>
-          <SearchCardTitle>
-            <SearchCardPlace>용산구</SearchCardPlace>
-            <SearchCardDate>2024/08/31</SearchCardDate>
-          </SearchCardTitle>
-          <SearchCardIntro>한줄소개</SearchCardIntro>
-          <SearchCardPeople>0명</SearchCardPeople>
-        </SearchCard>
-        <SearchCard>
-          <SearchCardTitle>
-            <SearchCardPlace>용산구</SearchCardPlace>
-            <SearchCardDate>2024/08/31</SearchCardDate>
-          </SearchCardTitle>
-          <SearchCardIntro>한줄소개</SearchCardIntro>
-          <SearchCardPeople>0명</SearchCardPeople>
-        </SearchCard>
-        <SearchPageSelectionLayout>
-
-        </SearchPageSelectionLayout>
-      </SearchSectionContainer>
-     </SearchSection>
+        <CalenderSection>
+          <CalendarTitle>
+            <SubTitle>마이요리사 찾기</SubTitle>
+            <SubTitleDesc>마이요리사님 일정에 맞는 홈파티를 찾아보세요!</SubTitleDesc>
+          </CalendarTitle>
+          <CalendarContainer>
+            <CalendarPart setSearchDate={setSearchDate}/>
+          </CalendarContainer>
+          <CalenderSearchBtn onClick={handleShowSearch}>검색</CalenderSearchBtn>
+        </CalenderSection>
+    { showSearchSec &&
+    <>
+      <SearchSection>
+        <Title title={'홈파티찾기'} subTitle={'요리사 모집 중인 홈파티에 신청해보세요!'} backgroundColor={'white'}></Title>
+        <SearchSectionContainer>
+          <SearchCard>
+            <SearchCardTitle>
+              <SearchCardPlace>용산구</SearchCardPlace>
+              <SearchCardDate>2024/08/31</SearchCardDate>
+            </SearchCardTitle>
+            <SearchCardIntro>한줄소개</SearchCardIntro>
+            <SearchCardPeople>0명</SearchCardPeople>
+          </SearchCard>
+          <SearchCard>
+            <SearchCardTitle>
+              <SearchCardPlace>용산구</SearchCardPlace>
+              <SearchCardDate>2024/08/31</SearchCardDate>
+            </SearchCardTitle>
+            <SearchCardIntro>한줄소개</SearchCardIntro>
+            <SearchCardPeople>0명</SearchCardPeople>
+          </SearchCard>
+          <SearchCard>
+            <SearchCardTitle>
+              <SearchCardPlace>용산구</SearchCardPlace>
+              <SearchCardDate>2024/08/31</SearchCardDate>
+            </SearchCardTitle>
+            <SearchCardIntro>한줄소개</SearchCardIntro>
+            <SearchCardPeople>0명</SearchCardPeople>
+          </SearchCard>
+          <SearchCard>
+            <SearchCardTitle>
+              <SearchCardPlace>용산구</SearchCardPlace>
+              <SearchCardDate>2024/08/31</SearchCardDate>
+            </SearchCardTitle>
+            <SearchCardIntro>한줄소개</SearchCardIntro>
+            <SearchCardPeople>0명</SearchCardPeople>
+          </SearchCard>
+          <SearchCard>
+            <SearchCardTitle>
+              <SearchCardPlace>용산구</SearchCardPlace>
+              <SearchCardDate>2024/08/31</SearchCardDate>
+            </SearchCardTitle>
+            <SearchCardIntro>한줄소개</SearchCardIntro>
+            <SearchCardPeople>0명</SearchCardPeople>
+          </SearchCard>
+          <SearchCard>
+            <SearchCardTitle>
+              <SearchCardPlace>용산구</SearchCardPlace>
+              <SearchCardDate>2024/08/31</SearchCardDate>
+            </SearchCardTitle>
+            <SearchCardIntro>한줄소개</SearchCardIntro>
+            <SearchCardPeople>0명</SearchCardPeople>
+          </SearchCard>
+          <SearchPageSelectionLayout>
+          </SearchPageSelectionLayout>
+        </SearchSectionContainer>
+      </SearchSection>
+    </>
+    }
      </ChefBoardContainer>
     </>
   )
@@ -78,20 +95,42 @@ export const ChefBoard = () => {
 const ChefBoardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 8%;
+  margin-bottom: 5%;
 `
 
-const CalenderContainer = styled.div`
+const CalenderSection= styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 90vh;
+  height: 100vh;
   background-color: ${(props)=>props.theme.sub};
 `
-const CalenderSection = styled.div`
+const CalendarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   height: 75vh;
+`
+const CalendarTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 80px;
+  gap: 10px;
+`
+const SubTitle = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    font-size: 23px;
+    font-weight: 600;
+`
+const SubTitleDesc = styled.div`
+      display: flex;
+    justify-content: center;
+    font-size: 14px;
 `
 
 const CalenderSearchBtn = styled.button`
