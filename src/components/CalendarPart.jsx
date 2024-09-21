1b72200f28558d4d4c24a95431997a666edaeb41
hwiy01 <vdocom02@naver.com>
2024-09-21 15:47:22 +0900

    [Feat] 고객 게시판 달력 날짜 선택 시 표시

--- src/components/CalendarPart.jsx
+++ src/components/CalendarPart.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 import styled from 'styled-components'
 
-export const CalendarPart = ({setSearchDate}) => {
+export const CalendarPart = ({ setSearchDate }) => {
+  const [selectedDates, setSelectedDates] = useState([]); // 선택된 날짜 str
+
   const handleDateClick = (dateObj) => {
-    //console.log(dateObj.dateStr);
+    const clickedDate = dateObj.dateStr;
+
+    setSelectedDates(dateObj.dateStr);
     setSearchDate(dateObj.dateStr);
-  }
+
+    //날짜가 이미 있으면 삭제, 없으면 추가
+    if(selectedDates.includes(dateObj.dateStr)){
+      setSelectedDates(selectedDates.filter(date => date != clickedDate))
+    }
+    else{
+      setSelectedDates([...selectedDates], clickedDate);
+    }
+
+    if (dateObj.dayEl.classList.contains('selected-date')) {
+      dateObj.dayEl.classList.remove('selected-date');
+    } else {
+      dateObj.dayEl.classList.add('selected-date');
+    }
+  };
+
 
   return (
     <CalendarContainer>
       <FullCalendar
-        plugins={[ dayGridPlugin, interactionPlugin ]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
-        dateClick={(dateObj)=>handleDateClick(dateObj)}
+        dateClick={handleDateClick}
       />
     </CalendarContainer>
-  )
-}
+  );
+};
 
 const CalendarContainer = styled.div`
   display: flex;
@@ -27,36 +46,54 @@ const CalendarContainer = styled.div`
   align-items: center;
   width: 770px;
 
-  //전체 컨테이너
-  .fc{
+  // 전체 컨테이너
+  .fc {
     width: 100%;
-    height: 70vh;
+    height: 72vh;
   }
 
-  //달력부분 컨테이너
-  .fc-theme-standard{
+  // 달력 부분 컨테이너
+  .fc-theme-standard {
     background-color: white;
     padding: 25px;
     border-radius: 25px;
   }
 
   // 달력 table border 없앰
-  .fc-scrollgrid{
+  .fc-scrollgrid {
     border: none;
   }
 
-  //요일 부분 하나 컨테이너
-  .fc-col-header th{
+  // 요일 부분 하나 컨테이너
+  .fc-col-header th {
     border: none;
   }
 
-  //날짜 하나 컨테이너
-  .fc-daygrid-day{
+  // 날짜 하나 컨테이너
+  .fc-daygrid-day {
     cursor: pointer;
     border: none;
-    :hover{
-      background-color: #D9D9D9;
-    }
+    padding: 5px;
 
+    &:hover {
+      background-color: #d9d9d9;
+    }
   }
-`
+
+  // 선택된 날짜 스타일링
+  .selected-date .fc-daygrid-day-number {
+    background-color: #fa7c15;
+    border-radius: 50%;
+    color: white;
+  }
+
+  // 기본 날짜 스타일링
+  .fc-daygrid-day-number {
+    display: inline-block;
+    padding: 4px;
+    width: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
