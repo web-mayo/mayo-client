dac577ab58a64407203682b2031def0ef1722473
hwiy01 <vdocom02@naver.com>
2024-09-22 20:51:32 +0900

    [Fix] 배포 경로 수정

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ import { ApiTest } from "./pages/ApiTest";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Routes>
+      <Route basename={process.env.PUBLIC_URL}>
         <Route path="/" element={<Layout />} theme={theme}>
           <Route index element={<Home />} theme={theme} />
           {/* api 테스트 */}
@@ -103,7 +103,7 @@ function App() {
             theme={theme}
           />
         </Route>
-      </Routes>
+      </Route>
     </ThemeProvider>
   );
 }
