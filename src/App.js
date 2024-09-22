9311a07b2489d3a943d74093ad6e262c022a4c59
hwiy01 <vdocom02@naver.com>
2024-09-22 19:06:47 +0900

    [Fix] 배포

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ import { ApiTest } from "./pages/ApiTest";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Routes basename={process.env.PUBLIC_URL}>
+      <Routes>
         <Route path="/" element={<Layout />} theme={theme}>
           <Route index element={<Home />} theme={theme} />
           {/* api 테스트 */}
