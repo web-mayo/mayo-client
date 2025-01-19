cfae5b61dc359bcdea1b8c3aa4b2a0ac5880a989
hwiy01 <vdocom02@naver.com>
2025-01-20 02:23:10 +0900

    [Feat] 요리사 방문 완료된 페이지 전체보기 UI

--- src/pages/chef/chefBoardPages/ChefCompletedEntire.jsx
+++ src/pages/chef/chefBoardPages/ChefCompletedEntire.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Title } from '../../../components/Title'
+import { useForm } from "react-hook-form";
+import moment from "moment";
+import { preventScroll } from '../../../modal/modal';
+import { HomePartyCardMatchFinished } from '../../../components/HomePartyCard';
+import { useNavigate } from 'react-router-dom';
 
 export const ChefCompletedEntire = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  setValue("startDate", moment("2024-06-30").format("YYYY-MM-DD"));
+  setValue("endDate", moment().add(1, "M").format("YYYY-MM-DD"));
+  const [selectedId, setSelectedId] = useState();
+  const navigate = useNavigate();
+  const [matchFinishedList, setMatchFinishedList] = useState([
+    {
+      "id": 0,
+      "info": "string",
+      "address": "string",
+      "scheduleAt": "string",
+      "createdAt": "string",
+      "budget": 0,
+      "capacity": "string"
+    },{
+      "id": 0,
+      "info": "string",
+      "address": "string",
+      "scheduleAt": "string",
+      "createdAt": "string",
+      "budget": 0,
+      "capacity": "string"
+    },{
+      "id": 0,
+      "info": "string",
+      "address": "string",
+      "scheduleAt": "string",
+      "createdAt": "string",
+      "budget": 0,
+      "capacity": "string"
+    },{
+      "id": 0,
+      "info": "string",
+      "address": "string",
+      "scheduleAt": "string",
+      "createdAt": "string",
+      "budget": 0,
+      "capacity": "string"
+    },{
+      "id": 0,
+      "info": "string",
+      "address": "string",
+      "scheduleAt": "string",
+      "createdAt": "string",
+      "budget": 0,
+      "capacity": "string"
+    }
+  ]);
+
+   const handleModal = (id) => {
+      const prevScroll = preventScroll();
+      //setPrevScrollY(prevScroll);
+      setSelectedId(id);  
+      //setModal(true);
+    }
+
   return (
-    <>ChefCom</>
+    <Container>
+      <CompletedContainer>
+        <Title title={'방문 완료된 홈파티'} backgroundcolor={'white'}/>
+        <DurationFilter>
+              검색 기간 설정
+              <StartInput>
+                <input type="date" {...register("startDate", {})} />
+              </StartInput>
+              ~
+              <EndInput>
+                <input type="date" {...register("endDate", {})} />
+              </EndInput>
+          </DurationFilter>
+          <CompletedList>
+              {matchFinishedList?.map(card=> (
+                <HomePartyCardMatchFinished 
+                id={card.id} 
+                info={card.info} 
+                scheduledAt={card.scheduleAt.substr(0,10)} 
+                text={"방문 완료"} 
+                onClick={()=>handleModal('completed', card.id)} 
+                bgColor={'#444444'} 
+                textColor={"white"}/>))
+              }
+          </CompletedList>
+        </CompletedContainer>
+        <GoReservePage onClick={()=>navigate('/reserve')}>매칭 내역 메인으로</GoReservePage>
+    </Container>
   )
 }
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 7%;
 
+`
+const CompletedContainer = styled.div`
+    width: 83%;
+    display: flex;
+    display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const DurationFilter = styled.div`
+  height: 58px;
+  display: flex;
+  padding-left: 8px;
+  align-items: center;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const StartInput = styled.div`
+  margin: 0 10px 0 30px;
+`;
+const EndInput = styled.div`
+  margin: 0 0 0 10px;
+`;
+
+const CompletedList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  row-gap: 30px;
+  column-gap: 20px;
+`
+const GoReservePage = styled.button`
+    margin-top: 5%;
+    background-color: #FA7C15;
+    border: none;
+    color: white;
+    width: 339px;
+    height: 48px;
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 600;
+    cursor: pointer;
+`
